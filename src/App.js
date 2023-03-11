eeb65f0e3cc64c152cf47451d6e70804059290e6
Marv <malvin.opondo@student.moringaschool.com>
2023-03-11 14:07:50 +0300

    feat(Styling): Create a background color for Navabar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+// import Home from "../src/Components/Home"
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* <img src={logo} className="App-logo" alt="logo" />    */}
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#techstars">TechStars</Nav.Link>
+            <Nav.Link href="#projects">Projects</Nav.Link>
+            <Nav.Link href="#contacts">Contacts</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Github</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">
+                Twitter
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Linkedin</NavDropdown.Item>
+              <NavDropdown.Divider />
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
       </header>
+      {/* <Home /> */}
     </div>
   );
 }
---
+++ src/Components/Home.js
@@ -0,0 +1,25 @@
+// import Home from "../Components/Home";
+// import { Container, Image } from 'react-bootstrap';
+
+
+// function Home() {
+//   return (
+//     <Container>
+//       <div className="d-flex align-items-center">
+//         <Image
+//           src="http://res.cloudinary.com/dtyyzvqn8/image/upload/v1678481869/My_Best_Shot_uyrwc1.jpg"
+//           alt="Placeholder Image"
+//           roundedCircle
+//           className="mr-3"
+//         />
+//         <div>
+//           <h2>FullStack Developer</h2>
+//           <p>Hi,<br></br>my name is Malvin Opondo,
+//           <br></br>i build things for the web!</p>
+//         </div>
+//       </div>
+//     </Container>
+//   );
+// }
+
+// export default Home
