4af9ac4cf44b581c0955ea2f33c418bf7f7be390
Marv <malvin.opondo@student.moringaschool.com>
2023-03-11 17:22:32 +0300

    feat(Footer): Create a footer component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,12 +1,14 @@
 import React from 'react'
+import background from "../Components/background.jpg";
 
 function Home() {
   return (
     <div className="home">
-      <h1>FullStack Developer</h1>
-      <div style={{ backgroundImage: `url(${background})` }}>
+      <h1>Hi,<br></br>I'm Malvin,<br></br>A FullStack Developer..</h1>
+      <p> I build amazing things for the web!</p>
+      <img src={background} alt='background' className='bg'/>
     </div>
-    </div>
+    
     
     
       
