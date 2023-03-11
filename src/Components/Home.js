873e3b7e1b7828fdb5bef82aac8c47cfc4888371
Marv <malvin.opondo@student.moringaschool.com>
2023-03-11 16:54:12 +0300

    feat(Home): Add background asset to the component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div className="home">
+      <h1>FullStack Developer</h1>
+      <div style={{ backgroundImage: `url(${background})` }}>
+    </div>
+    </div>
+    
+    
+      
+  
+    
   )
 }
 
-export default Home
+export default Home;
