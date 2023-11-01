f1d4025daea3c99cab5ad3094d4fb6991591e49a
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-11-01 14:49:55 +0300

    feat(About): create an about component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import Navbar from "../src/components/Navbar"
 import Home from "../src/components/Home"
+import About from "../src/components/About"
+
+
+
 function App() {
   return (
     <div>
         <Navbar />
         <Home />
+        <About />
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,12 +5,12 @@ function Home() {
   return (
     <div className='w-full h-screen bg-[#0a192f]'>
             {/* Container */}
-      <div className='max-w-[840px] mx-auto py-4 px-8 flex flex-col justify-center h-full'>
+      <div className='max-w-[740px] mx-auto px-3 pt-12 flex flex-col justify-center h-full'>
       <p className='text-pink-600'>Hi, my name is</p>
-        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>
+        <h1 className='text-4xl sm:text-5xl font-bold text-[#ccd6f6]'>
           MALVIN OCHIENG
         </h1>
-        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>
+        <h2 className='text-4xl sm:text-5xl font-bold text-[#8892b0]'>
           I'm a Full Stack Developer.
         </h2>
         <p className='text-[#8892b0] py-4 max-w-[800px]'>
