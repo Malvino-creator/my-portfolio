a4c2bc662d8cd1c36bc950769ed4198e7833117b
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-11-02 11:32:20 +0300

    feat(Works): Add an important tag to the index.css

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -13,17 +13,24 @@ function Work() {
           <p className="py-6">Check out my recent work</p>
         </div>
 
-        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+        <div style={{ backgroundImage: `url(${workImg})` }} className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
           <div
             className="shadow-lg shadow-[#040c16] group container rounded-md 
               flex justify-center text-center items-center mx-auto content-div"
           >
             {/* Hover Effects */}
-            <div>
-              <div>
-                <span></span>
-                <div></div>
-              </div>
+            <div className="opacity-0 group-hover:opacity-100">
+                 <span className="text-2xl font bold text-white tracking-wider">
+                    Reactjs Application
+                 </span>
+                <div className="pt-8 text-center">
+                    <a href="/">
+                    <button className="text-center rounded-lg px-4 py-3 m-2
+                       bg-white text-gray-700 font-bold text-lg">Code</button>
+                    </a>
+                    
+                </div>
+              
             </div>
           </div>
         </div>
