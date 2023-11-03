d4595605a9a5d90be2a19799ab19658995c2d83b
Malvin Opondo <malvin.opondo@student.moringaschool.com>
2023-11-03 14:30:04 +0300

    feat (debugging): Debug the whole website and add local data storage

---
+++ src/data/data.js
@@ -0,0 +1,50 @@
+import workImage from '../assets/workImg.jpeg'
+import realestate from '../assets/realestate.jpg'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:workImage,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:realestate,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:workImage,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:realestate,
+        github:"",
+        live:"",
+    },
+    {
+        id:5,
+        name:"React JS Application",
+        image:workImage,
+        github:"",
+        live:"",
+    },
+    {
+        id:6,
+        name:"React JS Application",
+        image:realestate,
+        github:"",
+        live:"",
+    },
+
+
+]
+
